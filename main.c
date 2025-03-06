52ec01bd77052c88595ef9f22bc2741d97e6b68a
laurirai <lauri.raisanen@outlook.com>
2025-03-06 12:18:13 +0200

    Start

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <SDL.h>
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+#define COLOR_WHITE 0xffffffff
+
+struct Circle {
+    double x;
+    double y;
+    double radius;
+};
+
+void drawCircle(SDL_Surface *surface, struct Circle circle) {
+    
+}
+
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window = SDL_CreateWindow( //Creating our "screen"
+        "Raytracer Demo", // name in top of the screen
+        SDL_WINDOWPOS_CENTERED, // x and y coordinate where the screen will b
+        SDL_WINDOWPOS_CENTERED, // but we call it to center on our own screen
+        SCREEN_WIDTH, //width
+        SCREEN_HEIGHT, // and height of the screen, but we've defined them already
+        SDL_WINDOW_SHOWN); // just to enable the screen to show
+
+
+    SDL_Surface *screen = SDL_GetWindowSurface(window);// retrieving the surface and saving it to a pointer
+    SDL_Rect rectangle = (SDL_Rect){200, 200, 100, 100};
+    SDL_FillRect(screen, &rectangle, COLOR_WHITE);
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Delay(5000); // testing for screen to b up
+    return 0;
+}
